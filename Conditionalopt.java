6c0d54a1e2fcc65bac66657624c4f5e40f43e89d
Sindhugoud03 <sindhugoudlingaram@gmail.com>
2025-01-28 09:50:43 +0530

    Create Conditionalopt.java

---
+++ Conditionalopt.java
@@ -0,0 +1,8 @@
+public class Operator {
+    public static void main(String[] args) {
+        int a =10, b = 5;
+        System.out.println("Addition:"+(a+b));
+        System.out.println("Relational"+(a=b));
+        System.out.println("Logical AND:"+(a>5 && b>2));
+    }
+}
